c2aa69dc8408a82927a1fb8ee6af23269217c981
Italo Motrib <ytaloribeiro@gmail.com>
2026-04-24 22:24:58 -0300

    Update updateEPI.cs

--- FormulaVisual/updateEPI.cs
+++ FormulaVisual/updateEPI.cs
@@ -14,3 +14,21 @@ private void fnUpDate_ExecuteCode(object sender, System.EventArgs args)
     throw new Exception("Erro ao executar a atualização: " + ex.Message);
   }
 }
+
+
+private void fnUpDate_ExecuteCode(object sender, System.EventArgs args) 
+{
+  try 
+  {
+    
+    string sql = "UPDATE VEPI SET CODLOC = '004', CODFILIAL = '5', " +
+                 "RECMODIFIEDON = GETDATE(), RECMODIFIEDBY = 'mestre' " +
+                 "WHERE CODCOLIGADA = 1 AND CODEPI ='EPI75079' AND EMPRESTADO IS NULL AND CODFILIAL<>5 AND CODLOC<>'004'";
+
+    this.DBS.QueryExec(sql);
+  } 
+  catch (Exception ex) 
+  {
+    throw new Exception("Erro ao executar a atualização: " + ex.Message);
+  }
+}
